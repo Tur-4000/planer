5271134da0427878c3f8f8e0280ecc66a1e2ca38
Turbanov <tur.4000@gmail.com>
2019-03-20 23:01:22 +0200

    add brand icon

--- planer/admin.py
+++ planer/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 
-from planer.models import Documents, TodoList, Category
+from planer.models import TodoList, Category
 
 
 class TodoListAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'due_date', 'end_date')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'color')
+
+
 admin.site.register(TodoList, TodoListAdmin)
-admin.site.register(Documents)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
