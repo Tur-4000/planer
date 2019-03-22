5fc266cfd4e7a3474ec79a5312f740a2f309fb8f
Turbanov <tur.4000@gmail.com>
2019-03-23 00:44:42 +0200

    прикрутил гайки

--- user/views.py
+++ user/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+# from django.contrib.auth.decorators import user_passes_test
 
 
-class RegisterView(LoginRequiredMixin, CreateView):
+# @user_passes_test(lambda user: user.is_superuser)
+class RegisterView(UserPassesTestMixin, CreateView):
     template_name = 'user/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('todo_list')
+
+    def test_func(self):
+        return self.request.user.is_superuser
 
 
 class UserLoginView(LoginView):
