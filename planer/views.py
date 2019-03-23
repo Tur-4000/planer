793628dd8ad8345fcf41e7eea5e8c8f275bd47f4
Turbanov <tur.4000@gmail.com>
2019-03-24 01:33:06 +0200

    fix: shaddow name next -> next_month

--- planer/views.py
+++ planer/views.py
@@ -156,9 +156,9 @@ def calendar(request, year, month):
     today = date.today()
     day = date(year, month, 1)
     prev = day + relativedelta(months=-1)
-    next = day + relativedelta(months=+1)
+    next_month = day + relativedelta(months=+1)
     my_tasks = TodoList.objects.order_by('due_date').filter(
         due_date__year=year, due_date__month=month, is_ended=False)
     cal = WorkoutCalendar(my_tasks).formatmonth(year, month)
-    context = {'calendar': mark_safe(cal), 'today': today, 'prev': prev, 'next': next}
+    context = {'calendar': mark_safe(cal), 'today': today, 'prev': prev, 'next': next_month}
     return render(request, 'planer/calendar.html', context)
